b69532dfe9559b5cd0ced73b53846e835f8d7b14
AlexMukha <alexmukha@email.com>
2019-01-31 14:45:03 -0800

    Update Init JS

---
+++ assets/javascript/train.js
@@ -0,0 +1,11 @@
+      // Initialize Firebase
+      var config = {
+        apiKey: "AIzaSyCrFeBUOky6zicH5Pgfvj_ssYWxm7cgUFI",
+        authDomain: "amfirebase-2e1f3.firebaseapp.com",
+        databaseURL: "https://amfirebase-2e1f3.firebaseio.com",
+        projectId: "amfirebase-2e1f3",
+        storageBucket: "amfirebase-2e1f3.appspot.com",
+        messagingSenderId: "153473692758"
+      };
+
+      firebase.initializeApp(config);
