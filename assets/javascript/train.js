b57b2c69e13d783dfa216c581562536b2fcf1202
AlexMukha <alexmukha@email.com>
2019-02-03 20:18:37 -0800

    Prevent submitting empty form

--- assets/javascript/train.js
+++ assets/javascript/train.js
@@ -13,11 +13,11 @@
       var currentTime = moment();
     //   var nowTime = moment(moment()).format("HH:mm");
     var timeIs = moment(currentTime).format("hh:mm");
-    console.log(timeIs);
+    // console.log(timeIs);
 
       var nhrs12 = moment(currentTime).format("hh");
       var nhrs24 = moment(currentTime).format("HH");
-      console.log("HRS are "+nhrs12+" and "+nhrs24);
+    //   console.log("HRS are "+nhrs12+" and "+nhrs24);
       if (nhrs12 === nhrs24) {
           var ntd = "AM";
       } else {
@@ -27,10 +27,21 @@
 
       // ON.Click function to add a new train
       $("#submit").on("click", function() {
+         
         var name = $("#trainNameInput").val().trim();
         var destin = $("#destinationInput").val().trim();
         var start = $("#timeInput").val().trim();
         var freq = $("#frequencyInput").val().trim();
+        // Make sure all fields are filled
+        if (name === "") {
+            console.log("Form is incomplete");
+        } else if (destin === "") {
+            console.log("Form is incomplete");
+        } else if (start === "") {
+            console.log("Form is incomplete");
+        } else if (freq === "") {
+            console.log("Form is incomplete");
+        } else {
 
     // Push data into a "Train" database
     var dbRef = database.ref().child("Trains");
@@ -40,13 +51,14 @@
     start: start,
     freq: freq
 });
+      }
 });
 
 
 
     // Pull data from "Train" database
     database.ref().child("Trains").on("child_added", function(snapshot) {
-            // Decaring a var db to hold an object
+        // Declaring a var db to hold an object
         var db = snapshot.val();
 
         // Time each train starts
@@ -59,45 +71,43 @@
     
         // First Time (pushed back 1 year to make sure it comes before current time)
         var startTimeFormat = moment(startTime, "HH:mm").subtract(1, "years");
-
-        // Current Time
-        // console.log(currentTime);
-        // console.log("Now is "+moment(currentTime).format("hh:mm"));
-      
+  
         //  Find difference between now and statTime
         var timeDiff = moment().diff(moment(startTimeFormat), "minutes");
-        console.log("Diff " + timeDiff+" min");
-        
+    
         // Find remainder
         var remainder = timeDiff % frequency;
-        console.log(remainder);
-
+     
         // Minute Until Train
-        var minTill = frequency - remainder;
-        console.log("Till " + minTill);       
+        var minTillTotal = frequency - remainder;
+       var hrsCalc = minTillTotal / 60;
+        var hrsOnly = Math.floor(hrsCalc);
+        var hrsTill = hrsOnly * 60;
+        var minTill = minTillTotal - hrsTill;
+         if (hrsTill < 1) {
+            var hrs = "";
+        } else {
+            var hrs = hrsOnly+"hrs ";
+        }
 
         // Next train 
-        var nextTrain = moment().add(minTill, "minutes");
+        var nextTrain = moment().add(minTillTotal, "minutes");
         var nextTime = moment(nextTrain).format("hh:mm");
+
+        // Assing AM or PM for the next trainn to arrive
         var hrs12 = moment(nextTrain).format("hh");
         var hrs24 = moment(nextTrain).format("HH");
-        console.log("HRS are "+hrs12+" and "+hrs24);
         if (hrs12 === hrs24) {
             var dt = "AM";
         } else {
             var dt = "PM"
         }
-
-
-
-
-        console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
-
+        // Append results to the page
         var $row = $("<tr>");
         $row.append("<td>"+db.name+"</td>");
         $row.append("<td>"+db.destin+"</td>");
         $row.append("<td class=\"text-center\">"+db.freq+"</td>");
         $row.append("<td class=\"text-center\">"+nextTime+" "+dt+"</td>");
-        $row.append("<td class=\"text-center\">"+minTill+"</td>");
+        $row.append("<td class=\"text-center\">"+hrs+minTill+"min</td>");
         $("#train-table-rows").append($row);
     })
